3471bacfd70822263390ff7a04b5852b75530290
MD Nazmul Hasa <mdnazmulhasanniloy323@gmai.com>
2022-12-24 21:38:11 -0800

    preloader is added

--- game-ghor-clint/src/App.js
+++ game-ghor-clint/src/App.js
@@ -2,15 +2,31 @@ import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { Router } from './Router/Router';
 import { Toaster } from 'react-hot-toast';
+import { useEffect, useState } from 'react';
+import HashLoader from 'react-spinners/HashLoader';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(()=>{
+      setLoading(false);
+
+    }, 8000)
+  },[])
   return (
-    <div className="max-w-[1440px] mx-auto">
+    <>
+   { 
+    loading? <div className="flex flex-col h-screen w-screen items-center justify-center"><HashLoader  color="#005be6" /> <h1 className='text-3xl font-bold'>welcome To Mobile Ghor</h1></div>
+
+   : <div className="max-w-[1440px] mx-auto">
       <RouterProvider router={Router}>
 
       </RouterProvider>
       <Toaster></Toaster>
     </div>
+  }
+    </>
   );
 }
 
